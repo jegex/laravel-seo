5ea524e617c2d90041f18287c37ab422c342b12b
jegex <hujan008@gmail.com>
2026-04-28 14:26:32 +0700

    configured

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Jegex\LaravelSeo\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use Jegex\LaravelSeo\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
